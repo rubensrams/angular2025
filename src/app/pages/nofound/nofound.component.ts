3c80805986f9118db9314b086616f0bbf8076695
rubensrams <rubensrams@hotmail.com>
2024-11-18 23:11:08 -0600

    18 11

--- src/app/pages/nofound/nofound.component.ts
+++ src/app/pages/nofound/nofound.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-nofound',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './nofound.component.html',
   styleUrl: './nofound.component.css'
 })
